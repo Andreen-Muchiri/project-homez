95a30c76c2df4c229e574aee54a6dbc6101b1512
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-05 19:32:44 +0300

    Add data component

---
+++ src/components/data/Data.js
@@ -0,0 +1,229 @@
+export const nav = [
+    {
+      text: "home",
+      path: "/",
+    },
+    {
+      text: "about",
+      path: "/about",
+    },
+    {
+      text: "services",
+      path: "/services",
+    },
+    {
+      text: "blog",
+      path: "/blog",
+    },
+    {
+      text: "pricing",
+      path: "/pricing",
+    },
+    {
+      text: "contact",
+      path: "/contact",
+    },
+  ]
+  export const featured = [
+    {
+      cover: "../images/hero/h1.png",
+      name: "3 Bedroom Apartments",
+      total: "122 Property",
+    },
+    {
+      cover: "../images/hero/h2.png",
+      name: "2 Bedroom Apartments      ",
+      total: "155 Property",
+    },
+    {
+      cover: "../images/hero/h3.png",
+      name: "1 Bedroom Apartments",
+      total: "300 Property",
+    },
+    {
+      cover: "../images/hero/h4.png",
+      name: "Studios",
+      total: "80 Property",
+    },
+    {
+      cover: "../images/hero/h6.png",
+      name: "Bedsitters",
+      total: "80 Property",
+    },
+  ]
+  export const list = [
+    {
+      id: 1,
+      cover: "../images/list/p-1.png",
+      name: "Red Carpet Real Estate",
+      location: "Kilimani, Nairobi",
+      category: "For Rent",
+      price: "Ksh 95,000",
+      type: "3 Bedroom Apartment En Suite",
+    },
+    {
+      id: 2,
+      cover: "../images/list/p-2.png",
+      name: "Fairmount Properties",
+      location: "Ruiru, Nairobi",
+      category: "For Rent",
+      price: "Ksh 30,000",
+      type: "1 Bedroom Apartment ",
+    },
+    {
+      id: 3,
+      cover: "../images/list/p-7.png",
+      name: "The Real Estate Corner",
+      location: "Dunga ,Kisumu",
+      category: "For Rent",
+      price: "Ksh 25,000",
+      type: "2 Bedroom Apartment",
+    },
+    {
+      id: 4,
+      cover: "../images/list/p-4.png",
+      name: "Herringbone Realty",
+      location: "Tom Mboya , Kisumu",
+      category: "For Rent",
+      price: "Ksh.10,000",
+      type: "Studios",
+    },
+    {
+      id: 5,
+      cover: "../images/list/p-5.png",
+      name: "Brick Lane Realty",
+      location: "Ongata Rongai,Kajiado",
+      category: "For Rent",
+      price: "Ksh.7,000",
+      type: "Bedsitter",
+    },
+    {
+      id: 6,
+      cover: "../images/list/p-6.png",
+      name: "Banyon Tree Realty",
+      location: "Maasai Lodge, Kajiado",
+      category: "For Rent",
+      price: "Ksh.12,000",
+      type: "2 Bedroom Apartment",
+    },
+  ]
+  export const awards = [
+    {
+      icon: <i class='fa-solid fa-trophy'></i>,
+      num: "32 M	",
+      name: "Real Estate 100 Award",
+    },
+    {
+      icon: <i class='fa-solid fa-briefcase'></i>,
+      num: "43 M",
+      name: "Kenya Real Estate Awards",
+    },
+    {
+      icon: <i class='fa-solid fa-lightbulb'></i>,
+      num: "51 M",
+      name: " Real Estate Excellence Award",
+    },
+    {
+      icon: <i class='fa-solid fa-heart'></i>,
+      num: "42 M",
+      name: "IITCA Green Award",
+    },
+  ]
+  export const location = [
+    {
+      id: 1,
+      name: "Nairobi ,Kilimani",
+      Apartments: "15 Apartments",
+      cover: "./images/location/city-1.png",
+    },
+    {
+      id: 2,
+      name: "Mombasa ",
+      Apartments: "10 Apartments",
+      cover: "./images/location/city-2.png",
+    },
+    {
+      id: 3,
+      name: "Kiambu",
+       Apartments: " 14 Apartments",
+       cover: "./images/location/city-3.png",
+    },
+    {
+      id: 4,
+      name: "Kisumu",
+      Apartments: " 18 Apartments",
+      cover: "./images/location/city-4.png",
+    },
+    {
+      id: 5,
+      name: "Kajiado",
+      Apartments: " 10 Apartments",
+      cover: "./images/location/city-5.png",
+    },
+    {
+      id: 6,
+      name: "Ruaka",
+      Apartments: " 10 Apartments",
+      cover: "./images/location/city-6.png",
+    },
+  ]
+  export const team = [
+    {
+      list: "50",
+      cover: "../images/customer/team-0.jpg",
+      address: "Ongata Rongai,Kajiado",
+      name: "Marylin Mwikali",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "70",
+      cover: "../images/customer/team-1.jpg",
+      address: "Kileleshwa,Nairobi",
+      name: "Hariet Nelly",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "80",
+      cover: "../images/customer/team-2.jpg",
+      address: "Kisumu",
+      name: "Anna Owens",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "51",
+      cover: "../images/customer/team-3.jpg",
+      address: "Diani,Mombasa",
+      name: "Michael Sultan",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "42",
+      cover: "../images/customer/team-4.jpg",
+      address: "Kiambu",
+      name: "Joseph Mmabo",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "38",
+      cover: "../images/customer/team-5.jpg",
+      address: "Mirema , Kahawa West",
+      name: "Adam James",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+  ]
+  
+  export const footer = [
+    {
+      title: "LAYOUTS",
+      text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property/Pricing Page" }, { list: "Contact Page" }, { list: "Blog" }],
+    },
+    // {
+    //   title: "ALL SECTIONS",
+    //   text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    // },
+    {
+      title: "COMPANY",
+      text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }],
+    },
+  ]
+  
